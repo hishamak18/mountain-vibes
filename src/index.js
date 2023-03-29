ff047cfecc6493e8c0aeb298bcbe4eee612029cc
hishamak18 <hishamak2004@gmail.com>
2023-03-29 06:45:59 +0530

    add routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Admin from "./pages/Admin";
+import AdminPannel from "./pages/AdminPannel";
+
 import AvailableRooms from "./pages/AvailableRoom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -15,9 +17,12 @@ export default function App() {
         <Route index Component={Home} />
           <Route path="/Reservation"Component={Resevation} />
           <Route path="/Admin"Component={Admin} />
+          <Route path="/AdminPannel"Component={AdminPannel} />
+
           <Route path="/AvailableRooms"Component={AvailableRooms} />
           <Route path="/Contact"Component={Contact} />
           <Route path="/About"Component={About} />
+
 
 
 
