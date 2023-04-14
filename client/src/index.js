4149fe5ab5d349e7cf5c22a056e8314dbb9c1249
Hishamak <111882586+hishamak18@users.noreply.github.com>
2023-04-14 13:36:08 +0530

    update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Admin from "./pages/Admin";
-import AdminPannel from "./pages/AdminPannel";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import AvailableRooms from "./pages/AvailableRoom";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Resevation from "./pages/Resevation";
-import SignIn from "./pages/SignIn";
-import AvailRooms from "./components/AvailRooms";
-
-
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index Component={Home} />
-          <Route path="/Reservation"Component={Resevation} />
-          <Route path="/Admin"Component={Admin} />
-          <Route path="/AdminSignInMountain"Component={SignIn} />
-
-          <Route path="/AdminPannel"Component={AdminPannel} />
-          <Route path="/AvailRooms"Component={AvailRooms} />
-
-          <Route path="/AvailableRooms"Component={AvailableRooms} />
-          <Route path="/Contact"Component={Contact} />
-          <Route path="/About"Component={About} />
-
-
-
-
-
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
