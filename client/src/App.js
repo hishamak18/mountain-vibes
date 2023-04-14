37b461a87bdc92e82fe1eda858e66e9f72f9e6a5
Hishamak <111882586+hishamak18@users.noreply.github.com>
2023-04-14 13:36:45 +0530

    added preloder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,51 @@
-import Home from "./pages/Home";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Preloader from './components/Preloader';
+import About from './pages/About';
+import Admin from './pages/Admin';
+import AdminPannel from './pages/AdminPannel';
+import AvailableRooms from './pages/AvailableRoom';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Resevation from './pages/Resevation';
+import SignIn from './pages/SignIn';
+import AvailRooms from './components/AvailRooms';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Set a timeout to simulate a loading delay
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    {loading ? (
+      <Preloader />
+    ) : (
+      <BrowserRouter>
+      <Routes>
+        <Route index Component={Home} />
+        <Route path="/Reservation" Component={Resevation} />
+        <Route path="/Admin" Component={Admin} />
+        <Route path="/AdminSignInMountain" Component={SignIn} />
+        <Route path="/AdminPannel" Component={AdminPannel} />
+        <Route path="/AvailRooms" Component={AvailRooms} />
+        <Route path="/AvailableRooms" Component={AvailableRooms} />
+        <Route path="/Contact" Component={Contact} />
+        <Route path="/About" Component={About} />
+      </Routes>
+    </BrowserRouter>
+    )}
+  </>
+   
   );
-}
+};
 
 export default App;
