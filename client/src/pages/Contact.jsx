68de26c86b3bfb06733bc34a7f61009a447eef6f
Hishamak <111882586+hishamak18@users.noreply.github.com>
2023-04-16 07:32:11 +0530

    changed contact bg img

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,12 +5,12 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import styled from "styled-components";
 import MobileMenu from "../components/MobileMenu"
+import contactimg from '../img/contact.svg';
+
 
 
 const Wrapper = styled.div`
-  background-image: url("https://images.pexels.com/photos/2748757/pexels-photo-2748757.jpeg?auto=compress&cs=tinysrgb&w=1600");
-  background-image: url(" https://images.pexels.com/photos/821754/pexels-photo-821754.jpeg?auto=compress&cs=tinysrgb&w=1600 ");
-
+  background-image: url(${contactimg});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
