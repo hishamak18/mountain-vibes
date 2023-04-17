36899821d343cbc8418df5511b5e189c382b25c0
Hishamak <111882586+hishamak18@users.noreply.github.com>
2023-04-17 07:38:07 +0530

    Update data.js

--- client/src/data.js
+++ client/src/data.js
@@ -1,6 +1,6 @@
  export const RoomImg=[
     {
-        img:"https://flic.kr/p/2otKdLq",
+        img:"https://live.staticflickr.com/65535/52822519592_e419af61e4_w.jpg",
         tittle:"Single Room"
         ,price:"1500INR/DAY"
     }
@@ -18,16 +18,16 @@
 
 export const images = [
 
-    { src: 'https://flic.kr/p/2otN9Sg', alt: 'Image 4' },
-     { src: 'https://flic.kr/p/2otKedY', alt: 'Image 4' },
-     { src: 'https://flic.kr/p/2otQoVE', alt: 'Image 4' },
-     { src: 'https://flic.kr/p/2otKdLq', alt: 'Image 2' },
-     { src: 'https://flic.kr/p/2otKdLR', alt: 'Image 2' },
+    { src: 'https://live.staticflickr.com/65535/52822521132_c8a70364f0_k.jpg', alt: 'Image 4' },
+     { src: 'https://live.staticflickr.com/65535/52823529328_8c1f778c4d_z.jpg', alt: 'Image 4' },
+     { src: 'https://live.staticflickr.com/65535/52822519617_6fd52eee5a_w.jpg', alt: 'Image 4' },
+     { src: 'https://live.staticflickr.com/65535/52823267339_7455332f1f_w.jpg', alt: 'Image 2' },
+     { src: 'https://live.staticflickr.com/65535/52823496665_2353abbf1d_w.jpg', alt: 'Image 2' },
 
     
-     { src: 'https://flic.kr/p/2otP43B', alt: 'Image 2' },
-    { src: 'https://flic.kr/p/2otQedv', alt: 'Image 3' },
-    { src: 'https://flic.kr/p/2otQeoR', alt: 'Image 4' },
-    { src: 'https://flic.kr/p/2otP42z', alt: 'Image 4' },
+     { src: 'https://live.staticflickr.com/65535/52823091811_461d919c26_z.jpg', alt: 'Image 2' },
+    { src: 'https://live.staticflickr.com/65535/52822519592_e419af61e4_w.jpg', alt: 'Image 3' },
+    { src: 'https://live.staticflickr.com/65535/52823529328_8c1f778c4d_z.jpg', alt: 'Image 4' },
+    { src: 'https://live.staticflickr.com/65535/52823496955_8a9d9acebd_z.jpg', alt: 'Image 4' },
   ];
   
