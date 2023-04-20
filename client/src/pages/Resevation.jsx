1f2e65eb379b68317ea57479b1ea05252f64c77c
Hishamak <111882586+hishamak18@users.noreply.github.com>
2023-04-20 13:50:08 +0530

    Update Resevation.jsx

--- client/src/pages/Resevation.jsx
+++ client/src/pages/Resevation.jsx
@@ -1,52 +1,37 @@
 import React from "react";
-
-import ContactForm from "../components/ContactForm";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import styled from "styled-components";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import ResortReservationForm from "../components/ResortReservationForm ";
 import MobileMenu from "../components/MobileMenu"
-import contactimg from '../img/contact.svg';
-
+import reservationimg from '../img/reservation.svg';
 
 
 const Wrapper = styled.div`
-<<<<<<< HEAD
-  background-image: url(${contactimg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  @media (max-width: 480px) {
-    background-position: -275px -10px;
-    padding-top:100px;
-
-
-  }
-`;
-
-const Contact = () => {
-=======
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  `;
-  const Container = styled.div`
   background-image: url(${reservationimg});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center center;
+`;
+const Container = styled.div`
   width: 100%;
   height: 100vh;
 `;
 const Reservation = () => {
->>>>>>> origin/main
   return (
-    <Wrapper>
-        <Navbar />
-        <MobileMenu/>
-        <ContactForm />
+    <Container>
+      <Navbar />
+      <MobileMenu/>
+      <Wrapper>
+        <ResortReservationForm />
+      </Wrapper>
       <Footer />
-    </Wrapper>
+    </Container>
   );
 };
 
-export default Contact;
+export default Reservation;
